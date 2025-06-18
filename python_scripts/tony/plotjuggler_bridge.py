c453a86fea40d374399f098b60f480e1951b60ac
tonyguzmann <161231403+tonyguzmann@users.noreply.github.com>
2025-06-18 09:40:59 -0500

    Update plotjuggler_bridge.py

--- python_scripts/tony/plotjuggler_bridge.py
+++ python_scripts/tony/plotjuggler_bridge.py
@@ -1,11 +1,14 @@
 # === plotjuggler_bridge.py ===
-import socket
-import json
+import socket # For sending data over network using UDP
+import json   # For formatting data as JSON
 import time
 
+# Create a UDP socket 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#address for PlotJuggler's UDP Server
 DESTINATION = ("127.0.0.1", 9870)
 
+# Function to send data to PlotJuggler 
 def send_to_plotjuggler(name, value):
     payload = {
         name: {
@@ -13,5 +16,8 @@ def send_to_plotjuggler(name, value):
             "value": value
         }
     }
+    # Convert Python into a JSON string
     msg = json.dumps(payload)
+    # Send the JSON to plotjuggler 
     sock.sendto(msg.encode(), DESTINATION)
+    
